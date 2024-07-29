150fc959cd80e5949f7533277481c4641e06c45f
ROWENA VASQUEZ <vasquezrd@gmail.com>
2024-07-29 18:34:56 +0200

    mywork

--- client/main.js
+++ client/main.js
@@ -1,20 +1,17 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", async function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const formData = new formData("form");
-  const formValues = Object.fromEntries(formData);
-  console.log(formValues);
+  const form = require("form");
+  const myObject = Object.fromEntries(data);
+  console.log(myObject);
 
-  const response = await fetch("http://localhost:8080/users", {
+  fetch("http://localhost:8080/users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(formValues),
+    body: JSON.stringify(myObject),
   });
-
-  const data = await response.json();
-  console.log(data);
 });
